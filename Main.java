a1ea38b1ed9c6105a393a95510eadc1a7176c88b
Mergezy <123715188+Mergezy@users.noreply.github.com>
2023-01-30 00:26:16 +0300

    upd

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for(byte i = 0; i <= 10; i++){
             for(byte g = 0; g <= 10; g++){
-                System.out.printf("%d * %d = %d",i,g,(i*g));
-                System.out.println("");
+                System.out.printf("%d * %d = %d \n",i,g,(i*g));
             }
             System.out.println("=========");
         }
